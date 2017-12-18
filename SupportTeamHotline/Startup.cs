4f2fb8075a90897bd6ee1edb8377d6604d5671e6
Mauricio Guzman <programmingrover@gmail.com>
2017-12-18 12:06:28 -0800

    Add project files.

---
+++ SupportTeamHotline/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SupportTeamHotline.Startup))]
+namespace SupportTeamHotline
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
